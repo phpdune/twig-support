2b46a740360f89267b32a61edba8c12a5f7ee91e
coderswat <coderswat@gmail.com>
2023-05-25 02:56:42 +0530

    updated docblock

--- tests/TwigExtensionTest.php
+++ tests/TwigExtensionTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Dune Framework.
+ *
+ * (c) Abhishek B <phpdune@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Dune\Support\Tests;
 
 use PHPUnit\Framework\TestCase;
